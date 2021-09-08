b0e82f10b15a051b2247c8933033c915986d72e4
Akn <sibata486@gmail.com>
2021-09-08 22:42:42 +0900

    init

---
+++ TimeStampNote/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace TimeStampNote
+{
+    using System.Windows;
+    using Prism.Ioc;
+    using TimeStampNote.Views;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ TimeStampNote/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TimeStampNote.ViewModels
+{
+    using Prism.Mvvm;
+
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Application";
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+    }
+}
