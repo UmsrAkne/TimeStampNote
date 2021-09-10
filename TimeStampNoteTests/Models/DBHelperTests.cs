dce3194442c0af27d710562ece3180c0cd4f102d
Akn <sibata486@gmail.com>
2021-09-10 22:10:44 +0900

    add / $B%F%9%H%W%m%8%'%/%H$rDI2C(B

---
+++ TimeStampNoteTests/Models/DBHelperTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TimeStampNote.Models.Tests
+{
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DBHelperTests
+    {
+        [TestMethod]
+        public void DBHelperTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
