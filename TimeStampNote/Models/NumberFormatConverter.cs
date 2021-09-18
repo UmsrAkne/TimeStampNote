381520f3018950b96bfbce6142ef0c4c5d1514a7
Akn <sibata486@gmail.com>
2021-09-18 17:51:16 +0900

    add / ID を ゼロ埋めして表示するためのコンバーターを追加、実装

---
+++ TimeStampNote/Models/NumberFormatConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace TimeStampNote.Models
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class NumberFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+             return string.Format("{0:00000}", (long)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
