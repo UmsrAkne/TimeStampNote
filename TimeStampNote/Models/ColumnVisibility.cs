cb0ad29743c9a3d51ef7f75a7613499a2a33d776
Akn <sibata486@gmail.com>
2021-09-19 23:15:09 +0900

    add / 列の表示、非表示の情報をまとめておくためのクラスを追加

---
+++ TimeStampNote/Models/ColumnVisibility.cs
@@ -0,0 +1,29 @@
+﻿namespace TimeStampNote.Models
+{
+    using Prism.Mvvm;
+
+    public class ColumnVisibility : BindableBase
+    {
+        private bool idColumn = true;
+        private bool dateColumn = true;
+        private bool orderNumberColumn = true;
+
+        public bool IDColumn
+        {
+            get => idColumn;
+            set => SetProperty(ref idColumn, value);
+        }
+
+        public bool DateColumn
+        {
+            get => dateColumn;
+            set => SetProperty(ref dateColumn, value);
+        }
+
+        public bool OrderNumberColumn
+        {
+            get => orderNumberColumn;
+            set => SetProperty(ref orderNumberColumn, value);
+        }
+    }
+}
