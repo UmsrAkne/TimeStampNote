ec82d62146876c0b4b9ff1c99df3926ded4f44eb
Akn <sibata486@gmail.com>
2021-10-15 22:44:13 +0900

    add / UIColors の各部の色の宣言を追加

--- TimeStampNote/Views/UIColors.cs
+++ TimeStampNote/Views/UIColors.cs
@@ -1,12 +1,67 @@
 ﻿namespace TimeStampNote.Views
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Windows.Media;
+    using Prism.Mvvm;
 
-    public class UIColors
+    public class UIColors : BindableBase
     {
+        private Theme theme;
+        private SolidColorBrush backgroundColorBrush;
+        private SolidColorBrush deepBackgroundColorBrush;
+        private SolidColorBrush foregroundColorBrush;
+        private SolidColorBrush borderColorBrush;
+
+        public UIColors(Theme defaultTheme)
+        {
+            Theme = defaultTheme;
+        }
+
+        public Theme Theme
+        {
+            get => theme;
+            set
+            {
+                theme = value;
+                if (theme == Theme.Dark)
+                {
+                    BackgroundColorBrush = new SolidColorBrush() { Color = Colors.DarkBlue };
+                    DeepBackgroundColorBrush = new SolidColorBrush() { Color = Colors.Black };
+                    ForegroundColorBrush = new SolidColorBrush() { Color = Colors.WhiteSmoke };
+                    BorderColorBrush = new SolidColorBrush() { Color = Colors.DarkGray };
+                }
+
+                if (theme == Theme.Light)
+                {
+                    BackgroundColorBrush = new SolidColorBrush() { Color = Colors.WhiteSmoke };
+                    DeepBackgroundColorBrush = new SolidColorBrush() { Color = Colors.White };
+                    ForegroundColorBrush = new SolidColorBrush() { Color = Colors.Black };
+                    BorderColorBrush = new SolidColorBrush() { Color = Colors.Gray };
+                }
+            }
+        }
+
+        public SolidColorBrush BackgroundColorBrush
+        {
+            get => backgroundColorBrush;
+            private set => SetProperty(ref backgroundColorBrush, value);
+        }
+
+        public SolidColorBrush DeepBackgroundColorBrush
+        {
+            get => deepBackgroundColorBrush;
+            private set => SetProperty(ref deepBackgroundColorBrush, value);
+        }
+
+        public SolidColorBrush ForegroundColorBrush
+        {
+            get => foregroundColorBrush;
+            private set => SetProperty(ref foregroundColorBrush, value);
+        }
+
+        public SolidColorBrush BorderColorBrush
+        {
+            get => borderColorBrush;
+            private set => SetProperty(ref borderColorBrush, value);
+        }
     }
 }
