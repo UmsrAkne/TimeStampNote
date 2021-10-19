fbc6a43d216b4843e26e868905d95678f3551809
Akn <sibata486@gmail.com>
2021-10-19 18:50:13 +0900

    add / Comment クラスにデータベースの構造を表すアノテーションを追記

--- TimeStampNote/Models/Comment.cs
+++ TimeStampNote/Models/Comment.cs
@@ -1,6 +1,8 @@
 ﻿namespace TimeStampNote.Models
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Comment
     {
@@ -10,23 +12,33 @@
         /// このオブジェクトを識別するユニークな ID です。
         /// デフォルト状態では "-1" が割り当てられており、この値が、ID 未割り当ての状態を表します。
         /// </summary>
+        [Key]
+        [Required]
+        [Column("id")]
         public long ID { get; set; } = -1;
 
         /// <summary>
         /// このオブジェクトを識別するユニークな ID (文字列）です。
         /// </summary>
+        [Column("sub_id")]
         public string SubID { get; set; }
 
+        [Column("text")]
         public string Text { get; set; }
 
+        [Column("posted_date")]
         public DateTime PostedDate { get; set; }
 
+        [Column("is_latest")]
         public bool IsLatest { get; set; }
 
+        [Column("group_name")]
         public string GroupName { get; set; }
 
+        [Column("order_number")]
         public long OrderNumber { get; set; }
 
+        [NotMapped]
         public bool IndexIsEven { get; set; }
 
         /// <summary>
