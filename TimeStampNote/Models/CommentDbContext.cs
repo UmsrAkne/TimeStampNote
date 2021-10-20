e2f76f9c2e5b3c35eda24064f9879bccc0520f77
Akn <sibata486@gmail.com>
2021-10-20 14:54:07 +0900

    add / DbContext クラスに Insert() を追加

--- TimeStampNote/Models/CommentDbContext.cs
+++ TimeStampNote/Models/CommentDbContext.cs
@@ -2,26 +2,40 @@
 {
     using Microsoft.Data.Sqlite;
     using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
     using System.Data.SQLite;
+    using System.IO;
+    using System.Linq;
 
     public class CommentDbContext : DbContext
     {
+        public string DBFileName => "Comments.sqlite";
 
         public DbSet<Comment> Comments { get; set; }
 
-        public CommentDbContext()
-        {
-        }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            SQLiteConnection.CreateFile("Comments.sqlite");
-            var connectionString = new SqliteConnectionStringBuilder { DataSource = @"Comments.sqlite" }.ToString();
+            if (!File.Exists(DBFileName))
+            {
+                SQLiteConnection.CreateFile(DBFileName);
+            }
+
+            var connectionString = new SqliteConnectionStringBuilder { DataSource = DBFileName }.ToString();
             optionsBuilder.UseSqlite(new SQLiteConnection(connectionString));
         }
 
-        public void insert()
+        public void CreateDatabase()
         {
+            if (!File.Exists(DBFileName))
+            {
+                Database.EnsureCreated();
+            }
+        }
+
+        public void Insert(List<Comment> comments)
+        {
+            comments.ForEach(c => Comments.Add(c));
+            SaveChanges();
         }
     }
 }
--- TimeStampNoteTests/Models/CommentDbContextTests.cs
+++ TimeStampNoteTests/Models/CommentDbContextTests.cs
@@ -1,18 +1,40 @@
-﻿
-namespace TimeStampNote.Models.Tests
+﻿namespace TimeStampNote.Models.Tests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using System.IO;
 
     [TestClass]
     public class CommentDbContextTests
     {
         [TestMethod]
-        public void insertTest()
+        public void InsertTest()
         {
+            var cc = new CommentDbContext();
+            string databaseFileName = cc.DBFileName;
+            cc.Dispose();
+
+            if (File.Exists(databaseFileName))
+            {
+                File.Delete(databaseFileName);
+            }
+
             using (var context = new CommentDbContext())
             {
-                context.Database.EnsureCreated();
+                context.CreateDatabase();
+
+                var comments = new List<Comment>()
+                {
+                    new Comment(){ ID = 2221, Text = "test1"},
+                    new Comment(){ ID = 2222 ,Text = "test2"},
+                    new Comment(){ ID = 2223 ,Text = "test3"}
+                };
+
+                context.Insert(comments);
+                databaseFileName = context.DBFileName;
             }
+
+            File.Delete(databaseFileName);
         }
     }
 }
