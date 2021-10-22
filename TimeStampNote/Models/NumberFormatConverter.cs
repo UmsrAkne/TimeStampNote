78578eec41d8401ef409da3fc1241892a60d7aa8
Akn <sibata486@gmail.com>
2021-10-22 19:16:40 +0900

    fix / NumberFormatConverter を使用時、表示する桁数を操作できるよう実装

--- TimeStampNote/Models/NumberFormatConverter.cs
+++ TimeStampNote/Models/NumberFormatConverter.cs
@@ -1,6 +1,7 @@
 ﻿namespace TimeStampNote.Models
 {
     using System;
+    using System.Linq;
     using System.Globalization;
     using System.Windows.Data;
 
@@ -8,7 +9,14 @@
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-             return string.Format("{0:00000}", (long)value);
+            if (parameter != null)
+            {
+                // "0" を指定回数分詰め込んだ配列を作成
+                var zeros = Enumerable.Range(1, int.Parse((string)parameter)).Select(n => "0").ToArray();
+                return string.Format("{0:" + string.Join("", zeros) + "}", (long)value);
+            }
+
+            return string.Format("{0:00000}", (long)value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
