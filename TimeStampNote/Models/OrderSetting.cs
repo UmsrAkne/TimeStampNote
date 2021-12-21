d82c18ef97070f1dc7eaf78dbcbcb71f25910c33
Akn <sibata486@gmail.com>
2021-12-21 23:57:01 +0900

    fix / SortColumnName に指定した値以外の文字列が入らないよう実装

--- TimeStampNote/Models/OrderSetting.cs
+++ TimeStampNote/Models/OrderSetting.cs
@@ -5,9 +5,13 @@
     public class OrderSetting : BindableBase
     {
         private bool reversing;
-        private string sortColumnName;
+        private string sortColumnName = "index";
 
-        public string SortColumnName { get => sortColumnName; set => SetProperty(ref sortColumnName, value); }
+        public string SortColumnName
+        {
+            get => sortColumnName;
+            set => SetProperty(ref sortColumnName, value == "date" || value == "id" ? value : "index");
+        }
 
         public bool Reversing { get => reversing; set => SetProperty(ref reversing, value); }
     }
